00b263ec83650f282f0dd222c00be9428cb8f0e0
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-08-02 22:04:33 +0900

    Add a03_select_device

---
+++ ash/src/bin/a03_select_device/main.rs
@@ -0,0 +1,101 @@
+//  03_select_device
+#[macro_use(defer)]
+extern crate scopeguard;
+use ash::version::{EntryV1_0, InstanceV1_0};
+use ash::vk::Handle;
+use vk_sample_config::config;
+
+fn main() {
+    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    unsafe {
+        let config = config::Configs::new("select_device");
+        let app_info = ash::vk::ApplicationInfo::builder()
+            .application_name(std::ffi::CString::new(config.prog_name).unwrap().as_c_str())
+            .application_version(ash::vk::make_version(1, 0, 0))
+            .engine_name(std::ffi::CStr::from_ptr(
+                "sample_engine\0".as_ptr() as *const std::os::raw::c_char
+            ))
+            .engine_version(ash::vk::make_version(1, 0, 0))
+            .api_version(ash::vk::make_version(1, 1, 0))
+            .build();
+
+        let entry = ash::Entry::new().unwrap();
+
+        let mut required_ext = std::collections::HashSet::<std::ffi::CString>::new();
+        for x in glfw.get_required_instance_extensions().unwrap() {
+            required_ext.insert(std::ffi::CString::new(x).unwrap());
+        }
+
+        let ext = glfw
+            .get_required_instance_extensions()
+            .unwrap()
+            .iter()
+            .map(|item| std::ffi::CString::new(item.as_str()).unwrap())
+            .collect::<Vec<_>>();
+        let ext_raw = &ext
+            .iter()
+            .map(|item| item.as_ptr())
+            .collect::<Vec<_>>();
+        let layers = if config.validation {
+            vec!["VK_LAYER_LUNARG_standard_validation\0".as_ptr() as *const i8]
+        } else {
+            vec![]
+        };
+
+        let create_info = ash::vk::InstanceCreateInfo::builder()
+            .application_info(&app_info)
+            .enabled_layer_names(&layers)
+            .enabled_extension_names(ext_raw.as_slice());
+
+        let instance = entry.create_instance(&create_info, None).unwrap();
+
+        defer! {
+            instance.destroy_instance(None);
+        }
+
+        let devices = instance.enumerate_physical_devices().unwrap();
+        if devices.len() == 0 {
+            eprintln!("利用可能なデバイスがない");
+            return;
+        }
+
+        let validated_devices = devices
+            .into_iter()
+            .filter(|device| {
+                let queue_props = instance.get_physical_device_queue_family_properties(*device);
+                for i in 0..queue_props.len() {
+                    if glfw.get_physical_device_presentation_support_raw(
+                        instance.handle().as_raw() as vk_sys::Instance,
+                        device.as_raw() as vk_sys::PhysicalDevice,
+                        i as u32,
+                    ) {
+                        return true;
+                    }
+                }
+                false
+            })
+            .collect::<Vec<_>>();
+
+        if validated_devices.len() == 0 {
+            eprintln!("必要な拡張とレイヤーを備えたデバイスがない");
+            return;
+        }
+
+        println!("利用可能なデバイス");
+        for i in 0..validated_devices.len() {
+            println!(
+                "{}: {}",
+                i,
+                std::ffi::CStr::from_ptr(
+                    instance
+                        .get_physical_device_properties(validated_devices[i])
+                        .device_name
+                        .as_ptr()
+                )
+                .to_str()
+                .unwrap()
+            )
+        }
+    }
+}
